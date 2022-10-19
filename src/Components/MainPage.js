2114ed37fa63148abe7eb3067a5397fc18e5075c
Ivan Gonzalez Valdes <kaosolution8@gmail.com>
2022-10-19 14:55:01 -0400

    firsr commit

---
+++ src/Components/MainPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Styles/MainPage.css";
+
+const MainPage = () => {
+  return (
+    <div className="mainPage">
+      <h1>Welcome to the website</h1>
+      <button>Click me</button>
+    </div>
+  );
+};
+
+export default MainPage;
